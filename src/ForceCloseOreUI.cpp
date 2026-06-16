bdd6bdd214e14889bd99cf8753794bbe0ee96cc1
Stivusik <ferfersilver@gmail.com>
2026-06-16 21:04:09 +0300

    26.40.22 test

--- src/ForceCloseOreUI.cpp
+++ src/ForceCloseOreUI.cpp
@@ -253,7 +253,7 @@ public:
 #elif __aarch64__
 #define OREUI_PATTERN                                                                     \
      std::initializer_list<const char *>({                                                \
-      "FF 03 06 D1 FD 7B 12 A9 FC 6F 13 A9 FA 67 14 A9 F8 5F 15 A9 F6 57 16 A9 F4 4F 17 A9 FD 83 04 91 49 D0 3B D5 F7 03 05 AA", \
+      "FF 03 06 D1 FD 7B 12 A9 FC 6F 13 A9 FA 67 14 A9 F8 5F 15 A9 F6 57 16 A9 F4 4F 17 A9 FD 83 04 91 5A D0 3B D5 F6 03 05 AA F5 03 03 2A", \
   })                                                                                                                    \
 
 #elif _WIN32
